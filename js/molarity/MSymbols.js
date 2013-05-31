63f2cb411850838aeb9cc118967a05c3ec049e0b
Chris Malley <cmalley@pixelzoom.com>
2013-05-31 16:38:51 -0600

    migrate ChemUtils to nitroglycerin.ChemUtils

--- js/molarity/MSymbols.js
+++ js/molarity/MSymbols.js
@@ -9,21 +9,21 @@ define( function( require ) {
   "use strict";
 
   // imports
-  var ChemUtils = require( "common/util/ChemUtils" );
+  var toSubscript = require( "NITROGLYCERIN/ChemUtils" ).toSubscript;
   var MStrings = require( "molarity/MStrings" );
 
   function MSymbols() {}
 
-  MSymbols.COBALT_II_NITRATE = ChemUtils.toSubscript( "Co(NO3)2" );
-  MSymbols.COBALT_CHLORIDE = ChemUtils.toSubscript( "CoCl2" );
-  MSymbols.COPPER_SULFATE = ChemUtils.toSubscript( "CuSO4" );
+  MSymbols.COBALT_II_NITRATE = toSubscript( "Co(NO3)2" );
+  MSymbols.COBALT_CHLORIDE = toSubscript( "CoCl2" );
+  MSymbols.COPPER_SULFATE = toSubscript( "CuSO4" );
   MSymbols.DRINK_MIX = MStrings.drinkMix;
-  MSymbols.GOLD_III_CHLORIDE = ChemUtils.toSubscript( "AuCl3" );
-  MSymbols.NICKEL_II_CHLORIDE = ChemUtils.toSubscript( "NiCl2" );
-  MSymbols.POTASSIUM_CHROMATE = ChemUtils.toSubscript( "K2CrO4" );
-  MSymbols.POTASSIUM_DICHROMATE = ChemUtils.toSubscript( "K2Cr2O7" );
-  MSymbols.POTASSIUM_PERMANGANATE = ChemUtils.toSubscript( "KMnO4" );
-  MSymbols.WATER = ChemUtils.toSubscript( "H2O" );
+  MSymbols.GOLD_III_CHLORIDE = toSubscript( "AuCl3" );
+  MSymbols.NICKEL_II_CHLORIDE = toSubscript( "NiCl2" );
+  MSymbols.POTASSIUM_CHROMATE = toSubscript( "K2CrO4" );
+  MSymbols.POTASSIUM_DICHROMATE = toSubscript( "K2Cr2O7" );
+  MSymbols.POTASSIUM_PERMANGANATE = toSubscript( "KMnO4" );
+  MSymbols.WATER = toSubscript( "H2O" );
 
   return MSymbols;
 } );
