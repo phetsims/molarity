7963cbf1d7b49a7394df1968126c453bddf6abfa
Chris Malley <cmalley@pixelzoom.com>
2013-07-24 12:37:08 -0600

    use imageLoader as import name to be consistent with other copies of ResetAllButton

--- js/molarity/view/ResetAllButton.js
+++ js/molarity/view/ResetAllButton.js
@@ -8,17 +8,17 @@ define( function( require ) {
   'use strict';
 
   // imports
-  var MImages = require( 'molarity/MImages' );
+  var imageLoader = require( 'molarity/MImages' );
   var Image = require( 'SCENERY/nodes/Image' );
   var inherit = require( 'PHET_CORE/inherit' );
   var PushButton = require( 'SUN/PushButton' );
 
   function ResetAllButton( callback, options ) {
     PushButton.call( this,
-      new Image( MImages.getImage( 'reset_button_up.png' ) ),
-      new Image( MImages.getImage( 'reset_button_over.png' ) ),
-      new Image( MImages.getImage( 'reset_button_down.png' ) ),
-      new Image( MImages.getImage( 'reset_button_disabled.png' ) ),
+      new Image( imageLoader.getImage( 'reset_button_up.png' ) ),
+      new Image( imageLoader.getImage( 'reset_button_over.png' ) ),
+      new Image( imageLoader.getImage( 'reset_button_down.png' ) ),
+      new Image( imageLoader.getImage( 'reset_button_disabled.png' ) ),
       callback, options );
   }
 
