cf00eb3d520bc78a029d8e7f22787b0800bb6e7c
Chris Malley <cmalley@pixelzoom.com>
2013-07-30 12:37:43 -0600

    make particleColor an option for Solute, not a constructor param

--- js/molarity/model/Solute.js
+++ js/molarity/model/Solute.js
@@ -16,13 +16,13 @@ define( function( require ) {
    * @param {Number} saturatedConcentration M (moles/L)
    * @param {Color} minColor solution color for smallest non-zero concentration
    * @param {Color} maxColor solution color for saturated concentration
-   * @param {Color} particleColor the solute's color as a particle, defaults to maxColor
    * @param {*} options
    * @constructor
    */
-  function Solute( name, formula, saturatedConcentration, minColor, maxColor, particleColor, options ) {
+  function Solute( name, formula, saturatedConcentration, minColor, maxColor, options ) {
 
     options = _.extend( {
+      particleColor: maxColor, // the solute's color as a particle
       particleSize: 5,    // particles are square, this is the length of one side
       particlesPerMole: 200 // number of particles to show per mol of saturation
     }, options );
@@ -32,7 +32,7 @@ define( function( require ) {
     this.saturatedConcentration = saturatedConcentration;
     this.minColor = minColor;
     this.maxColor = maxColor;
-    this.particleColor = particleColor || maxColor;
+    this.particleColor = options.particleColor;
     this.particleSize = options.particleSize;
     this.particlesPerMole = options.particlesPerMole;
   }
