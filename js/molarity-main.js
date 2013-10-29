6bb36921540697e142a575eb76a275d44538da39
Chris Malley <cmalley@pixelzoom.com>
2013-10-29 11:30:00 -0600

    joist#69, convert to different requirejs style for main.js

--- js/molarity-main.js
+++ js/molarity-main.js
@@ -5,28 +5,34 @@
  *
  * @author Chris Malley (PixelZoom, Inc.)
  */
-require( [ 'JOIST/SimLauncher', 'JOIST/Sim', 'MOLARITY/molarity/MolarityScreen', 'string!MOLARITY/molarity.name' ],
-  function( SimLauncher, Sim, MolarityScreen, simTitle ) {
-    'use strict';
+define( function( require ) {
+  'use strict';
 
-    //TODO i18n, see joist#66
-    var simOptions = {
-      credits: 'PhET Development Team -\n' +
-               'Lead Design: Julia Chamberlain\n' +
-               'Software Development: Chris Malley\n' +
-               'Design Team: Kelly Lancaster, Robert Parson, Kathy Perkins'
-    };
+  // imports
+  var MolarityScreen = require( 'MOLARITY/molarity/MolarityScreen' );
+  var Sim = require( 'JOIST/Sim' );
+  var SimLauncher = require( 'JOIST/SimLauncher' );
 
-    // Appending '?dev' to the URL will enable developer-only features.
-    if ( window.phetcommon.getQueryParameter( 'dev' ) ) {
-      simOptions = _.extend( {
-        // add dev-specific options here
-      }, simOptions );
-    }
+  // strings
+  var simTitle = require( 'string!MOLARITY/molarity.name' );
 
-    SimLauncher.launch( function() {
-      var sim = new Sim( simTitle, [ new MolarityScreen() ], simOptions );
-      sim.start();
-    } );
+  //TODO i18n, see joist#66
+  var simOptions = {
+    credits: 'PhET Development Team -\n' +
+             'Lead Design: Julia Chamberlain\n' +
+             'Software Development: Chris Malley\n' +
+             'Design Team: Kelly Lancaster, Robert Parson, Kathy Perkins'
+  };
 
+  // Appending '?dev' to the URL will enable developer-only features.
+  if ( window.phetcommon.getQueryParameter( 'dev' ) ) {
+    simOptions = _.extend( {
+      // add dev-specific options here
+    }, simOptions );
+  }
+
+  SimLauncher.launch( function() {
+    var sim = new Sim( simTitle, [ new MolarityScreen() ], simOptions );
+    sim.start();
   } );
+} );
