9d0281ea54d98f46334b20d8e0cd76a9df641c7b
jbphet <john.blanco@colorado.edu>
2014-05-07 17:50:32 -0700

    Changed to the new version of the ResetAllButton, see https://github.com/phetsims/sun/issues/94.

--- js/molarity/view/MolarityView.js
+++ js/molarity/view/MolarityView.js
@@ -20,7 +20,7 @@ define( function( require ) {
   var PhetFont = require( 'SCENERY_PHET/PhetFont' );
   var PrecipitateNode = require( 'MOLARITY/molarity/view/PrecipitateNode' );
   var Property = require( 'AXON/Property' );
-  var ResetAllButtonDeprecated = require( 'SCENERY_PHET/ResetAllButtonDeprecated' );
+  var ResetAllButton = require( 'SCENERY_PHET/ResetAllButton' );
   var SaturatedIndicator = require( 'MOLARITY/molarity/view/SaturatedIndicator' );
   var ScreenView = require( 'JOIST/ScreenView' );
   var Shape = require( 'KITE/Shape' );
@@ -101,9 +101,12 @@ define( function( require ) {
     showValuesCheckBox.touchArea = Shape.rectangle( showValuesCheckBox.left, showValuesCheckBox.top - 15, showValuesCheckBox.width, showValuesCheckBox.height + 30 );
 
     // Reset All button
-    var resetAllButton = new ResetAllButtonDeprecated( function() {
-      valuesVisibleProperty.reset();
-      model.reset();
+    var resetAllButton = new ResetAllButton( {
+      listener: function() {
+        valuesVisibleProperty.reset();
+        model.reset();
+      },
+      scale: 1.32
     } );
 
     // rendering order
