3454743999b21c9dbb6121deca2456b075936446
Chris Malley <cmalley@pixelzoom.com>
2014-08-19 13:25:39 -0600

    phetsims/joist#147 set ScreenView layoutBounds via options constructor parameter

--- js/molarity/view/MolarityView.js
+++ js/molarity/view/MolarityView.js
@@ -54,7 +54,7 @@ define( function( require ) {
   function MolarityView( model ) {
 
     var thisView = this;
-    ScreenView.call( thisView, { renderer: 'svg' } );
+    ScreenView.call( thisView, { renderer: 'svg', layoutBounds: new Bounds2( 0, 0, 1100, 700 ) } );
 
     var valuesVisibleProperty = new Property( false );
 
@@ -159,5 +159,5 @@ define( function( require ) {
     }
   }
 
-  return inherit( ScreenView, MolarityView, { layoutBounds: new Bounds2( 0, 0, 1100, 700 ) } );
+  return inherit( ScreenView, MolarityView );
 } );
