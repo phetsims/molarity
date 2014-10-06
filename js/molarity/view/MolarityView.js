e574ab951658a0f0774badd03f923024e2e5c3ed
Chris Malley <cmalley@pixelzoom.com>
2014-10-06 13:28:32 -0600

    phetsims/scenery-phet#66 move all buttons into scenery-phet.buttons

--- js/molarity/view/MolarityView.js
+++ js/molarity/view/MolarityView.js
@@ -20,7 +20,7 @@ define( function( require ) {
   var PhetFont = require( 'SCENERY_PHET/PhetFont' );
   var PrecipitateNode = require( 'MOLARITY/molarity/view/PrecipitateNode' );
   var Property = require( 'AXON/Property' );
-  var ResetAllButton = require( 'SCENERY_PHET/ResetAllButton' );
+  var ResetAllButton = require( 'SCENERY_PHET/buttons/ResetAllButton' );
   var SaturatedIndicator = require( 'MOLARITY/molarity/view/SaturatedIndicator' );
   var ScreenView = require( 'JOIST/ScreenView' );
   var Shape = require( 'KITE/Shape' );
