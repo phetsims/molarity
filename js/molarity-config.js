dd2f1963eaf717abdcda6ab1cb0efa375e1a1733
samreid <reids@colorado.edu>
2016-04-13 13:30:53 -0600

    Moved TandemDragHandler to tandem/ and added tandem to config, see https://github.com/phetsims/tandem/issues/9

--- js/molarity-config.js
+++ js/molarity-config.js
@@ -33,6 +33,7 @@ require.config( {
     SCENERY: '../../scenery/js',
     SCENERY_PHET: '../../scenery-phet/js',
     SUN: '../../sun/js',
+    TANDEM: '../../tandem/js',
 
     // sim code
     MOLARITY: '.'
