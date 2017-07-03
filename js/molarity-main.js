7f7f642ac32ee0fdd1997445a2814aed9008bd6d
Chris Malley <cmalley@pixelzoom.com>
2017-07-03 10:38:51 -0600

    1.3: update QA credits, #33

--- js/molarity-main.js
+++ js/molarity-main.js
@@ -25,7 +25,7 @@ define( function( require ) {
       leadDesign: 'Julia Chamberlain',
       softwareDevelopment: 'Chris Malley (PixelZoom, Inc.)',
       team: 'Kelly Lancaster, Robert Parson, Kathy Perkins',
-      qualityAssurance: 'Steele Dalton, Elise Morgan, Oliver Orejola, Bryan Yoelin'
+      qualityAssurance: 'Steele Dalton, Alex Dornan, Ethan Johnson,\nElise Morgan, Oliver Orejola, Bryan Yoelin'
     }
   };
 
