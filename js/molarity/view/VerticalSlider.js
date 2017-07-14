ef7673d67c4f92d9d4c4e0598e5229869820609d
Chris Malley <cmalley@pixelzoom.com>
2017-07-14 14:43:04 -0600

    doc #40

--- js/molarity/view/VerticalSlider.js
+++ js/molarity/view/VerticalSlider.js
@@ -70,7 +70,7 @@ define( function( require ) {
       maxLabel, valuesVisibleProperty, RANGE_FONT, tandem.createTandem( 'maxNode' ),
       { maxWidth: MAX_TEXT_WIDTH } );
 
-    // Use an HSlider rotated -90 degrees
+    // Use an HSlider rotated -90 degrees, see https://github.com/phetsims/molarity/issues/40
     var sliderNode = new HSlider( property, range, {
       trackSize: new Dimension2( trackSize.height, trackSize.width ), // swap dimensions
       trackFillEnabled: 'black',
