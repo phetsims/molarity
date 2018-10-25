2b0abc42762c519fe8ec3d930bafcb0b47b5420c
zepumph <michael.kauzmann@colorado.edu>
2018-10-25 13:02:46 -0800

    move phetioInherit to tandem, https://github.com/phetsims/phet-io/issues/1401

--- js/molarity/model/SoluteIO.js
+++ js/molarity/model/SoluteIO.js
@@ -12,11 +12,11 @@ define( function( require ) {
   // modules
   var molarity = require( 'MOLARITY/molarity' );
   var ObjectIO = require( 'TANDEM/types/ObjectIO' );
+  var phetioInherit = require( 'TANDEM/phetioInherit' );
 
   // ifphetio
   var assertInstanceOf = require( 'ifphetio!PHET_IO/assertInstanceOf' );
   var phetio = require( 'ifphetio!PHET_IO/phetio' );
-  var phetioInherit = require( 'ifphetio!PHET_IO/phetioInherit' );
 
   /**
    * @param {Solute} solute
