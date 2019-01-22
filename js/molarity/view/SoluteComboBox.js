51911528236f01f6ca4c48936cd6f77775312d44
Chris Malley <cmalley@pixelzoom.com>
2019-01-22 13:45:55 -0700

    fixed for various a11y options, https://github.com/phetsims/sun/issues/314

--- js/molarity/view/SoluteComboBox.js
+++ js/molarity/view/SoluteComboBox.js
@@ -42,7 +42,7 @@ define( function( require ) {
         highlightFill: 'rgb( 218, 255, 255 )',
 
         // a11y
-        a11yButtonLabel: 'Solute'
+        buttonLabelContent: 'Solute'
       }, options );
 
       assert && assert( !options.tandem, 'tandem is a required constructor parameter' );
