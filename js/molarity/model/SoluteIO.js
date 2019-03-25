5cf4537de3bd6d65965ae21b24880d84a40027b7
Chris Malley <cmalley@pixelzoom.com>
2019-03-25 11:30:50 -0600

    grunt update-copyright-dates

--- js/molarity/model/SoluteIO.js
+++ js/molarity/model/SoluteIO.js
@@ -1,4 +1,4 @@
-// Copyright 2017-2018, University of Colorado Boulder
+// Copyright 2017-2019, University of Colorado Boulder
 
 //TODO replace this with StateObjectIO, https://github.com/phetsims/molarity/issues/54
 /**
