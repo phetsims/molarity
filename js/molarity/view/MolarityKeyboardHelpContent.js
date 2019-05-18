b1d0fba9741c3c7279ba4b2bd1831c1b9df367c5
twant <taylor.want@gmail.com>
2019-05-18 16:36:47 -0700

    #57 #89 add spacing and change wording of keyboard help dialog

--- js/molarity/view/MolarityKeyboardHelpContent.js
+++ js/molarity/view/MolarityKeyboardHelpContent.js
@@ -24,7 +24,7 @@ define( function( require ) {
   // strings
   const keyboardSliderHelpHeadingString = require( 'string!MOLARITY/keyboard.sliderHelpHeading' );
   const keyboardPopUpListString = require( 'string!MOLARITY/keyboard.popUpList' );
-  const keyboardChangeChooseString = require( 'string!MOLARITY/keyboard.changeChoose' );
+  const keyboardChangeSoluteString = require( 'string!MOLARITY/keyboard.changeSolute' );
   const keyboardMoveThroughString = require( 'string!MOLARITY/keyboard.moveThrough' );
   const keyboardCloseListString = require( 'string!MOLARITY/keyboard.closeList' );
   const keyboardChangeSoluteHelpHeadingString = require( 'string!MOLARITY/keyboard.changeSoluteHelpHeading' );
@@ -60,16 +60,16 @@ define( function( require ) {
       // change solute help section
       const step1 = KeyboardHelpSection.labelWithIcon( keyboardPopUpListString, ICON_CREATOR.enterOrSpace(), popUpListDescriptionString );
       const step2 = KeyboardHelpSection.labelWithIcon( keyboardMoveThroughString, ICON_CREATOR.upOrDown(), moveThroughDescriptionString );
-      const step3 = KeyboardHelpSection.labelWithIcon( keyboardChangeChooseString, ICON_CREATOR.enter(), changeChooseDescriptionString );
+      const step3 = KeyboardHelpSection.labelWithIcon( keyboardChangeSoluteString, ICON_CREATOR.enter(), changeChooseDescriptionString );
       const step4 = KeyboardHelpSection.labelWithIcon( keyboardCloseListString, ICON_CREATOR.esc(), closeListDescriptionString );
       const changeSoluteContent = [ step1, step2, step3, step4 ];
 
-      const changeSoluteHelpSection = new KeyboardHelpSection( keyboardChangeSoluteHelpHeadingString, changeSoluteContent );
+      const changeSoluteHelpSection = new KeyboardHelpSection( keyboardChangeSoluteHelpHeadingString, changeSoluteContent);
       KeyboardHelpSection.alignHelpSectionIcons( [ generalNavigationHelpSection, changeSoluteHelpSection ] );
 
       const rightContent = new VBox( {
         children: [ changeSoluteHelpSection, generalNavigationHelpSection ],
-        spacing: 10,
+        spacing: 20,
         align: 'left'
       } );
       super( sliderKeyboardHelpSection, rightContent, { spacing: 35 } );
