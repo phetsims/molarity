60a76905f90aa3ad854ee4767a57c4fcce51693f
zepumph <michael.kauzmann@colorado.edu>
2019-12-13 16:23:41 -0900

    create UtteranceQueue.removeUtterance and use it to remove sliderUtterance sometimes, https://github.com/phetsims/molarity/issues/184

--- js/molarity/view/molarityAlertManager.js
+++ js/molarity/view/molarityAlertManager.js
@@ -137,7 +137,7 @@ define( require => {
       // sliderUtterance must be removed from the queue because it is otherwise possible to get stale slider alerts that no
       // longer make sense with the current saturation state.
       if ( utteranceQueue.hasUtterance( this.sliderUtterance ) ) {
-        utteranceQueue.clear();
+        utteranceQueue.removeUtterance( this.sliderUtterance );
       }
       this.saturationUtterance.alert = this.precipitateAmountDescriber.getSaturationChangedString();
       utteranceQueue.addToFront( this.saturationUtterance );
