b319946ee103e445dfd8cf1fd41dbcf157f2afc1
Jonathan Olson <jonathan.olson@colorado.edu>
2019-12-31 11:20:27 -0700

    Improvement for molarity ES6-module conversion

--- js/molarity/view/SoluteSelectionSoundGenerator.js
+++ js/molarity/view/SoluteSelectionSoundGenerator.js
@@ -13,16 +13,27 @@ define( require => {
   const MultiClip = require( 'TAMBO/sound-generators/MultiClip' );
 
   // sounds
+  const compoundWLowPassFilter001Sound = require( 'sound!MOLARITY/compound-w-low-pass-filter-001.mp3' );
+  const compoundWLowPassFilter002Sound = require( 'sound!MOLARITY/compound-w-low-pass-filter-002.mp3' );
+  const compoundWLowPassFilter003Sound = require( 'sound!MOLARITY/compound-w-low-pass-filter-003.mp3' );
+  const compoundWLowPassFilter004Sound = require( 'sound!MOLARITY/compound-w-low-pass-filter-004.mp3' );
+  const compoundWLowPassFilter005Sound = require( 'sound!MOLARITY/compound-w-low-pass-filter-005.mp3' );
+  const compoundWLowPassFilter006Sound = require( 'sound!MOLARITY/compound-w-low-pass-filter-006.mp3' );
+  const compoundWLowPassFilter007Sound = require( 'sound!MOLARITY/compound-w-low-pass-filter-007.mp3' );
+  const compoundWLowPassFilter008Sound = require( 'sound!MOLARITY/compound-w-low-pass-filter-008.mp3' );
+  const compoundWLowPassFilter009Sound = require( 'sound!MOLARITY/compound-w-low-pass-filter-009.mp3' );
+
+  // sounds
   const selectionSounds = [
-    require( 'sound!MOLARITY/compound-w-low-pass-filter-001.mp3' ),
-    require( 'sound!MOLARITY/compound-w-low-pass-filter-002.mp3' ),
-    require( 'sound!MOLARITY/compound-w-low-pass-filter-003.mp3' ),
-    require( 'sound!MOLARITY/compound-w-low-pass-filter-004.mp3' ),
-    require( 'sound!MOLARITY/compound-w-low-pass-filter-005.mp3' ),
-    require( 'sound!MOLARITY/compound-w-low-pass-filter-006.mp3' ),
-    require( 'sound!MOLARITY/compound-w-low-pass-filter-007.mp3' ),
-    require( 'sound!MOLARITY/compound-w-low-pass-filter-008.mp3' ),
-    require( 'sound!MOLARITY/compound-w-low-pass-filter-009.mp3' )
+    compoundWLowPassFilter001Sound,
+    compoundWLowPassFilter002Sound,
+    compoundWLowPassFilter003Sound,
+    compoundWLowPassFilter004Sound,
+    compoundWLowPassFilter005Sound,
+    compoundWLowPassFilter006Sound,
+    compoundWLowPassFilter007Sound,
+    compoundWLowPassFilter008Sound,
+    compoundWLowPassFilter009Sound
   ];
 
   class SoluteSelectionSoundGenerator extends MultiClip {
