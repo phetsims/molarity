570fa062bd7efba95797f15a99452bfd9942910f
samreid <reids@colorado.edu>
2020-01-31 08:51:15 -0700

    Update slider enabledRange phetioDocumentation, see https://github.com/phetsims/gravity-and-orbits/issues/293

--- js/phet-io/molarity-phet-io-elements-baseline.js
+++ js/phet-io/molarity-phet-io-elements-baseline.js
@@ -4591,7 +4591,7 @@ window.phet.phetio.phetioElementsBaseline = assert &&
       "phetioTypeName": "PropertyIO<BooleanIO>"
     },
     "molarity.molarityScreen.view.soluteAmountSlider.sliderNode.enabledRangeProperty": {
-      "phetioDocumentation": "",
+      "phetioDocumentation": "Sliders support two ranges: the outer range which specifies the min and max of the track and the enabledRangeProperty, which determines how low and high the thumb can be dragged within the track.",
       "phetioDynamicElement": false,
       "phetioEventType": "MODEL",
       "phetioFeatured": false,
@@ -6463,7 +6463,7 @@ window.phet.phetio.phetioElementsBaseline = assert &&
       "phetioTypeName": "PropertyIO<BooleanIO>"
     },
     "molarity.molarityScreen.view.solutionVolumeSlider.sliderNode.enabledRangeProperty": {
-      "phetioDocumentation": "",
+      "phetioDocumentation": "Sliders support two ranges: the outer range which specifies the min and max of the track and the enabledRangeProperty, which determines how low and high the thumb can be dragged within the track.",
       "phetioDynamicElement": false,
       "phetioEventType": "MODEL",
       "phetioFeatured": false,
