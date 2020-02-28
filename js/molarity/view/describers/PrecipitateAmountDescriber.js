ce8e0da9d9d811d6177185a34eefc141df6b34f6
phet-dev <phetadmin@gmail.com>
2020-02-28 03:34:54 -0700

    update copyright dates from daily grunt work

--- js/molarity/view/describers/PrecipitateAmountDescriber.js
+++ js/molarity/view/describers/PrecipitateAmountDescriber.js
@@ -1,4 +1,4 @@
-// Copyright 2019, University of Colorado Boulder
+// Copyright 2019-2020, University of Colorado Boulder
 
 /**
  * PrecipitateAmountDescriber is responsible for formulating strings about Solution.precipitateAmountProperty. This
