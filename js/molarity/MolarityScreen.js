3f990e98925721ab184e45c0f7d4d515b5128e74
pixelzoom <cmalley@pixelzoom.com>
2020-09-25 15:50:00 -0600

    convert Screen subclasses to ES6 class, https://github.com/phetsims/tasks/issues/1044

--- js/molarity/MolarityScreen.js
+++ js/molarity/MolarityScreen.js
@@ -1,35 +1,34 @@
 // Copyright 2013-2020, University of Colorado Boulder
 
 /**
- * The 'Molarity' screen. Conforms to the contract specified in joist/Screen.
+ * The 'Molarity' screen.
  *
  * @author Chris Malley (PixelZoom, Inc.)
  */
 
 import Screen from '../../../joist/js/Screen.js';
-import inherit from '../../../phet-core/js/inherit.js';
 import molarity from '../molarity.js';
 import MolarityModel from './model/MolarityModel.js';
 import MolarityScreenView from './view/MolarityScreenView.js';
 
-/**
- * @param {Tandem} tandem
- * @constructor
- */
-function MolarityScreen( tandem ) {
+class MolarityScreen extends Screen {
 
-  const options = {
-    tandem: tandem
-  };
+  /**
+   * @param {Tandem} tandem
+   */
+  constructor( tandem ) {
 
-  Screen.call( this,
-    function() { return new MolarityModel( tandem.createTandem( 'model' ) ); },
-    function( model ) { return new MolarityScreenView( model, tandem.createTandem( 'view' ) ); },
-    options
-  );
+    const options = {
+      tandem: tandem
+    };
+
+    super(
+      function() { return new MolarityModel( tandem.createTandem( 'model' ) ); },
+      function( model ) { return new MolarityScreenView( model, tandem.createTandem( 'view' ) ); },
+      options
+    );
+  }
 }
 
 molarity.register( 'MolarityScreen', MolarityScreen );
-
-inherit( Screen, MolarityScreen );
 export default MolarityScreen;
