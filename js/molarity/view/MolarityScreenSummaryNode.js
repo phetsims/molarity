58a2da2deac73591a0d51c20ef9bf67691e21d4d
phet-dev <phetadmin@gmail.com>
2021-11-26 03:18:58 -0700

    update copyright dates from daily grunt work

--- js/molarity/view/MolarityScreenSummaryNode.js
+++ js/molarity/view/MolarityScreenSummaryNode.js
@@ -1,4 +1,4 @@
-// Copyright 2019-2020, University of Colorado Boulder
+// Copyright 2019-2021, University of Colorado Boulder
 
 /**
  * Node that holds the PDOM content for the screen summary in Molarity.
