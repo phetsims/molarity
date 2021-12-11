7fe99947c17ed1a457e1a81f74339ec8305078f9
Sam Reid <reids@colorado.edu>
2021-12-10 17:17:18 -0700

    grunt update, see https://github.com/phetsims/chipper/issues/1170

--- js/molarityStrings.ts
+++ js/molarityStrings.ts
@@ -3,11 +3,240 @@
 /**
  * Auto-generated from modulify, DO NOT manually modify.
  */
-
+/* eslint-disable */
 import getStringModule from '../../chipper/js/getStringModule.js';
 import molarity from './molarity.js';
 
-const molarityStrings = getStringModule( 'MOLARITY' );
+type StringsType = {
+  'molarity': {
+    'label': string,
+    'title': string
+  },
+  'full': string,
+  'high': string,
+  'liters': string,
+  'low': string,
+  'lots': string,
+  'moles': string,
+  'none': string,
+  'saturated': string,
+  'showValues': string,
+  'solute': string,
+  'soluteAmount': string,
+  'solutionConcentration': string,
+  'solutionVolume': string,
+  'units': {
+    'liters': string,
+    'molarity': string,
+    'moles': string
+  },
+  'zero': string,
+  'cobaltIINitrate': string,
+  'cobaltChloride': string,
+  'copperSulfate': string,
+  'drinkMix': string,
+  'goldIIIChloride': string,
+  'nickelIIChloride': string,
+  'potassiumChromate': string,
+  'potassiumDichromate': string,
+  'potassiumPermanganate': string,
+  'pattern': {
+    '0label': string,
+    '0value': {
+      '1units': string
+    },
+    'parentheses': {
+      '0text': string
+    }
+  },
+  'solutes': string,
+  'soluteLowercase': string,
+  'keyboard': {
+    'sliderHelpHeading': string,
+    'changeSoluteHelpHeading': string
+  },
+  'a11y': {
+    'of': string,
+    'more': {
+      'capitalized': string,
+      'lowercase': string
+    },
+    'less': {
+      'capitalized': string,
+      'lowercase': string
+    },
+    'soluteAmountNoNewline': string,
+    'beakerSolutionControls': string,
+    'solutionValuesHelpText': string,
+    'sliderHelpText': string,
+    'soluteComboBoxHelpText': string,
+    'screenSummary': {
+      'playAreaPattern': string,
+      'controlAreaPattern': string,
+      'currentStateOfSimPattern': string,
+      'currentStateOfSimNoSolutePattern': string,
+      'simInteractionHint': string,
+      'quantitativeConcentrationPattern': string,
+      'qualitativeConcentrationPattern': string
+    },
+    'beaker': {
+      'pureWater': string,
+      'waterFormulaDescription': string,
+      'header': string,
+      'descriptionPattern': string,
+      'descriptionPureWaterPattern': string,
+      'hasZeroConcentration': string,
+      'soluteAmountPattern': string,
+      'saturationPattern': string,
+      'chemicalFormulaPattern': string,
+      'concentrationRangePattern': string
+    },
+    'saturated': string,
+    'notSaturated': string,
+    'quantitative': {
+      'solutionVolumeAndUnitPattern': string,
+      'hasVolumePattern': string,
+      'soluteAmountAndUnitPattern': string,
+      'saturatedValueTextPattern': string,
+      'concentrationStatePattern': string,
+      'concentrationAndUnit': string,
+      'sliderAlertPattern': string
+    },
+    'qualitative': {
+      'saturatedValueTextPattern': string,
+      'volumeStatePattern': string,
+      'soluteAmountStatePattern': string,
+      'concentrationStateClausePattern': string,
+      'sliderAlertPattern': string
+    },
+    'noSoluteAlert': string,
+    'quantityChange': {
+      'volumeChangePattern': string,
+      'soluteAmountChangedPattern': string,
+      'concentrationChangePattern': string,
+      'colorChangePattern': string,
+      'lighter': string,
+      'darker': string
+    },
+    'soluteChangedUnsaturatedAlertPattern': string,
+    'soluteChangedSaturatedAlertPattern': string,
+    'soluteChangedQualitativeConcentrationPattern': string,
+    'soluteChangedQuantitativeConcentrationPattern': string,
+    'noSoluteAlertQuantitative': string,
+    'noSoluteAlertQualitative': string,
+    'atMaxConcentrationPattern': string,
+    'atMaxConcentrationAlertPattern': string,
+    'saturationReachedAlertPattern': string,
+    'saturationLostQualitativeAlertPattern': string,
+    'saturationLostQuantitativeAlertPattern': string,
+    'saturationLostNoSoluteAlert': string,
+    'stillSaturatedAlertPattern': string,
+    'withSolidsAlertPattern': string,
+    'solidsChangePattern': string,
+    'solutionValuesCheckedAlert': string,
+    'solutionValuesUncheckedAlert': string,
+    'volumeRegions': {
+      'active': {
+        'isFull': string,
+        'isNearlyFull': string,
+        'isOverHalfFull': string,
+        'isHalfFull': string,
+        'isUnderHalfFull': string,
+        'isAtLowestAmount': string,
+        'isNearlyEmpty': string
+      },
+      'passive': {
+        'full': string,
+        'nearlyFull': string,
+        'overHalfFull': string,
+        'halfFull': string,
+        'underHalfFull': string,
+        'nearlyEmpty': string,
+        'atLowestAmount': string
+      }
+    },
+    'soluteAmountRegions': {
+      'maxAmountOf': string,
+      'aLotOf': string,
+      'aBunchOf': string,
+      'some': string,
+      'aTinyBitOf': string,
+      'aLittle': string,
+      'no': string
+    },
+    'precipitateAmountRegions': {
+      'lowercase': {
+        'aCoupleOf': string,
+        'aFew': string,
+        'some': string,
+        'aBunchOf': string,
+        'aLotOf': string
+      },
+      'capitalized': {
+        'aCoupleOf': string,
+        'aFew': string,
+        'some': string,
+        'aBunchOf': string,
+        'aLotOf': string
+      }
+    },
+    'concentrationRegions': {
+      'active': {
+        'hasZeroConcentration': string,
+        'hasLowConcentration': string,
+        'isSlightlyConcentrated': string,
+        'isNotVeryConcentrated': string,
+        'isVeryConcentrated': string,
+        'isHighlyConcentrated': string,
+        'hasMaxConcentration': string
+      },
+      'passive': {
+        'zeroConcentration': string,
+        'lowConcentration': string,
+        'slightlyConcentrated': string,
+        'notVeryConcentrated': string,
+        'veryConcentrated': string,
+        'highlyConcentrated': string,
+        'maxConcentration': string
+      }
+    },
+    'soluteColors': {
+      'capitalized': {
+        'red': string,
+        'pink': string,
+        'orange': string,
+        'gold': string,
+        'yellow': string,
+        'green': string,
+        'blue': string,
+        'purple': string,
+        'clear': string
+      },
+      'lowercase': {
+        'red': string,
+        'pink': string,
+        'orange': string,
+        'gold': string,
+        'yellow': string,
+        'green': string,
+        'blue': string,
+        'purple': string,
+        'clear': string
+      }
+    },
+    'cobaltIINitrateLowercase': string,
+    'cobaltChlorideLowercase': string,
+    'copperSulfateLowercase': string,
+    'drinkMixLowercase': string,
+    'goldIIIChlorideLowercase': string,
+    'nickelIIChlorideLowercase': string,
+    'potassiumChromateLowercase': string,
+    'potassiumDichromateLowercase': string,
+    'potassiumPermanganateLowercase': string
+  }
+};
+
+const molarityStrings = getStringModule( 'MOLARITY' ) as StringsType;
 
 molarity.register( 'molarityStrings', molarityStrings );
 
