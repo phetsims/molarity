7eb9d8bae320897c209986fc199bf77c96534107
Sam Reid <reids@colorado.edu>
2022-05-07 09:36:26 -0600

    Use semicolons to terminate fields in type StringType, see https://github.com/phetsims/chipper/issues/1208

--- js/molarityStrings.ts
+++ js/molarityStrings.ts
@@ -9,231 +9,231 @@ import molarity from './molarity.js';
 
 type StringsType = {
   'molarity': {
-    'label': string,
-    'title': string
-  },
-  'full': string,
-  'high': string,
-  'liters': string,
-  'low': string,
-  'lots': string,
-  'moles': string,
-  'none': string,
-  'saturated': string,
-  'showValues': string,
-  'solute': string,
-  'soluteAmount': string,
-  'solutionConcentration': string,
-  'solutionVolume': string,
+    'label': string;
+    'title': string;
+  };
+  'full': string;
+  'high': string;
+  'liters': string;
+  'low': string;
+  'lots': string;
+  'moles': string;
+  'none': string;
+  'saturated': string;
+  'showValues': string;
+  'solute': string;
+  'soluteAmount': string;
+  'solutionConcentration': string;
+  'solutionVolume': string;
   'units': {
-    'liters': string,
-    'molarity': string,
-    'moles': string
-  },
-  'zero': string,
-  'cobaltIINitrate': string,
-  'cobaltChloride': string,
-  'copperSulfate': string,
-  'drinkMix': string,
-  'goldIIIChloride': string,
-  'nickelIIChloride': string,
-  'potassiumChromate': string,
-  'potassiumDichromate': string,
-  'potassiumPermanganate': string,
+    'liters': string;
+    'molarity': string;
+    'moles': string;
+  };
+  'zero': string;
+  'cobaltIINitrate': string;
+  'cobaltChloride': string;
+  'copperSulfate': string;
+  'drinkMix': string;
+  'goldIIIChloride': string;
+  'nickelIIChloride': string;
+  'potassiumChromate': string;
+  'potassiumDichromate': string;
+  'potassiumPermanganate': string;
   'pattern': {
-    '0label': string,
+    '0label': string;
     '0value': {
-      '1units': string
-    },
+      '1units': string;
+    };
     'parentheses': {
-      '0text': string
+      '0text': string;
     }
-  },
-  'solutes': string,
-  'soluteLowercase': string,
+  };
+  'solutes': string;
+  'soluteLowercase': string;
   'keyboard': {
-    'sliderHelpHeading': string,
-    'changeSoluteHelpHeading': string
-  },
+    'sliderHelpHeading': string;
+    'changeSoluteHelpHeading': string;
+  };
   'a11y': {
-    'of': string,
+    'of': string;
     'more': {
-      'capitalized': string,
-      'lowercase': string
-    },
+      'capitalized': string;
+      'lowercase': string;
+    };
     'less': {
-      'capitalized': string,
-      'lowercase': string
-    },
-    'soluteAmountNoNewline': string,
-    'soluteValuePattern': string,
-    'beakerSolutionControls': string,
-    'solutionValuesHelpText': string,
-    'sliderHelpText': string,
-    'soluteComboBoxHelpText': string,
+      'capitalized': string;
+      'lowercase': string;
+    };
+    'soluteAmountNoNewline': string;
+    'soluteValuePattern': string;
+    'beakerSolutionControls': string;
+    'solutionValuesHelpText': string;
+    'sliderHelpText': string;
+    'soluteComboBoxHelpText': string;
     'screenSummary': {
-      'playAreaPattern': string,
-      'controlAreaPattern': string,
-      'currentStateOfSimPattern': string,
-      'currentStateOfSimNoSolutePattern': string,
-      'simInteractionHint': string,
-      'quantitativeConcentrationPattern': string,
-      'qualitativeConcentrationPattern': string
-    },
+      'playAreaPattern': string;
+      'controlAreaPattern': string;
+      'currentStateOfSimPattern': string;
+      'currentStateOfSimNoSolutePattern': string;
+      'simInteractionHint': string;
+      'quantitativeConcentrationPattern': string;
+      'qualitativeConcentrationPattern': string;
+    };
     'beaker': {
-      'pureWater': string,
-      'waterFormulaDescription': string,
-      'header': string,
-      'descriptionPattern': string,
-      'descriptionPureWaterPattern': string,
-      'hasZeroConcentration': string,
-      'soluteAmountPattern': string,
-      'saturationPattern': string,
-      'chemicalFormulaPattern': string,
-      'concentrationRangePattern': string
-    },
-    'saturated': string,
-    'notSaturated': string,
+      'pureWater': string;
+      'waterFormulaDescription': string;
+      'header': string;
+      'descriptionPattern': string;
+      'descriptionPureWaterPattern': string;
+      'hasZeroConcentration': string;
+      'soluteAmountPattern': string;
+      'saturationPattern': string;
+      'chemicalFormulaPattern': string;
+      'concentrationRangePattern': string;
+    };
+    'saturated': string;
+    'notSaturated': string;
     'quantitative': {
-      'solutionVolumeAndUnitPattern': string,
-      'hasVolumePattern': string,
-      'soluteAmountAndUnitPattern': string,
-      'saturatedValueTextPattern': string,
-      'concentrationStatePattern': string,
-      'concentrationAndUnit': string,
-      'sliderAlertPattern': string
-    },
+      'solutionVolumeAndUnitPattern': string;
+      'hasVolumePattern': string;
+      'soluteAmountAndUnitPattern': string;
+      'saturatedValueTextPattern': string;
+      'concentrationStatePattern': string;
+      'concentrationAndUnit': string;
+      'sliderAlertPattern': string;
+    };
     'qualitative': {
-      'saturatedValueTextPattern': string,
-      'volumeStatePattern': string,
-      'soluteAmountStatePattern': string,
-      'concentrationStateClausePattern': string,
-      'sliderAlertPattern': string
-    },
-    'noSoluteAlert': string,
+      'saturatedValueTextPattern': string;
+      'volumeStatePattern': string;
+      'soluteAmountStatePattern': string;
+      'concentrationStateClausePattern': string;
+      'sliderAlertPattern': string;
+    };
+    'noSoluteAlert': string;
     'quantityChange': {
-      'volumeChangePattern': string,
-      'soluteAmountChangedPattern': string,
-      'concentrationChangePattern': string,
-      'colorChangePattern': string,
-      'lighter': string,
-      'darker': string
-    },
-    'soluteChangedUnsaturatedAlertPattern': string,
-    'soluteChangedSaturatedAlertPattern': string,
-    'soluteChangedQualitativeConcentrationPattern': string,
-    'soluteChangedQuantitativeConcentrationPattern': string,
-    'noSoluteAlertQuantitative': string,
-    'noSoluteAlertQualitative': string,
-    'atMaxConcentrationPattern': string,
-    'atMaxConcentrationAlertPattern': string,
-    'saturationReachedAlertPattern': string,
-    'saturationLostQualitativeAlertPattern': string,
-    'saturationLostQuantitativeAlertPattern': string,
-    'saturationLostNoSoluteAlert': string,
-    'stillSaturatedAlertPattern': string,
-    'withSolidsAlertPattern': string,
-    'solidsChangePattern': string,
-    'solutionValuesCheckedAlert': string,
-    'solutionValuesUncheckedAlert': string,
+      'volumeChangePattern': string;
+      'soluteAmountChangedPattern': string;
+      'concentrationChangePattern': string;
+      'colorChangePattern': string;
+      'lighter': string;
+      'darker': string;
+    };
+    'soluteChangedUnsaturatedAlertPattern': string;
+    'soluteChangedSaturatedAlertPattern': string;
+    'soluteChangedQualitativeConcentrationPattern': string;
+    'soluteChangedQuantitativeConcentrationPattern': string;
+    'noSoluteAlertQuantitative': string;
+    'noSoluteAlertQualitative': string;
+    'atMaxConcentrationPattern': string;
+    'atMaxConcentrationAlertPattern': string;
+    'saturationReachedAlertPattern': string;
+    'saturationLostQualitativeAlertPattern': string;
+    'saturationLostQuantitativeAlertPattern': string;
+    'saturationLostNoSoluteAlert': string;
+    'stillSaturatedAlertPattern': string;
+    'withSolidsAlertPattern': string;
+    'solidsChangePattern': string;
+    'solutionValuesCheckedAlert': string;
+    'solutionValuesUncheckedAlert': string;
     'volumeRegions': {
       'active': {
-        'isFull': string,
-        'isNearlyFull': string,
-        'isOverHalfFull': string,
-        'isHalfFull': string,
-        'isUnderHalfFull': string,
-        'isAtLowestAmount': string,
-        'isNearlyEmpty': string
-      },
+        'isFull': string;
+        'isNearlyFull': string;
+        'isOverHalfFull': string;
+        'isHalfFull': string;
+        'isUnderHalfFull': string;
+        'isAtLowestAmount': string;
+        'isNearlyEmpty': string;
+      };
       'passive': {
-        'full': string,
-        'nearlyFull': string,
-        'overHalfFull': string,
-        'halfFull': string,
-        'underHalfFull': string,
-        'nearlyEmpty': string,
-        'atLowestAmount': string
+        'full': string;
+        'nearlyFull': string;
+        'overHalfFull': string;
+        'halfFull': string;
+        'underHalfFull': string;
+        'nearlyEmpty': string;
+        'atLowestAmount': string;
       }
-    },
+    };
     'soluteAmountRegions': {
-      'maxAmountOf': string,
-      'aLotOf': string,
-      'aBunchOf': string,
-      'some': string,
-      'aTinyBitOf': string,
-      'aLittle': string,
-      'no': string
-    },
+      'maxAmountOf': string;
+      'aLotOf': string;
+      'aBunchOf': string;
+      'some': string;
+      'aTinyBitOf': string;
+      'aLittle': string;
+      'no': string;
+    };
     'precipitateAmountRegions': {
       'lowercase': {
-        'aCoupleOf': string,
-        'aFew': string,
-        'some': string,
-        'aBunchOf': string,
-        'aLotOf': string
-      },
+        'aCoupleOf': string;
+        'aFew': string;
+        'some': string;
+        'aBunchOf': string;
+        'aLotOf': string;
+      };
       'capitalized': {
-        'aCoupleOf': string,
-        'aFew': string,
-        'some': string,
-        'aBunchOf': string,
-        'aLotOf': string
+        'aCoupleOf': string;
+        'aFew': string;
+        'some': string;
+        'aBunchOf': string;
+        'aLotOf': string;
       }
-    },
+    };
     'concentrationRegions': {
       'active': {
-        'hasZeroConcentration': string,
-        'hasLowConcentration': string,
-        'isSlightlyConcentrated': string,
-        'isNotVeryConcentrated': string,
-        'isVeryConcentrated': string,
-        'isHighlyConcentrated': string,
-        'hasMaxConcentration': string
-      },
+        'hasZeroConcentration': string;
+        'hasLowConcentration': string;
+        'isSlightlyConcentrated': string;
+        'isNotVeryConcentrated': string;
+        'isVeryConcentrated': string;
+        'isHighlyConcentrated': string;
+        'hasMaxConcentration': string;
+      };
       'passive': {
-        'zeroConcentration': string,
-        'lowConcentration': string,
-        'slightlyConcentrated': string,
-        'notVeryConcentrated': string,
-        'veryConcentrated': string,
-        'highlyConcentrated': string,
-        'maxConcentration': string
+        'zeroConcentration': string;
+        'lowConcentration': string;
+        'slightlyConcentrated': string;
+        'notVeryConcentrated': string;
+        'veryConcentrated': string;
+        'highlyConcentrated': string;
+        'maxConcentration': string;
       }
-    },
+    };
     'soluteColors': {
       'capitalized': {
-        'red': string,
-        'pink': string,
-        'orange': string,
-        'gold': string,
-        'yellow': string,
-        'green': string,
-        'blue': string,
-        'purple': string,
-        'clear': string
-      },
+        'red': string;
+        'pink': string;
+        'orange': string;
+        'gold': string;
+        'yellow': string;
+        'green': string;
+        'blue': string;
+        'purple': string;
+        'clear': string;
+      };
       'lowercase': {
-        'red': string,
-        'pink': string,
-        'orange': string,
-        'gold': string,
-        'yellow': string,
-        'green': string,
-        'blue': string,
-        'purple': string,
-        'clear': string
+        'red': string;
+        'pink': string;
+        'orange': string;
+        'gold': string;
+        'yellow': string;
+        'green': string;
+        'blue': string;
+        'purple': string;
+        'clear': string;
       }
-    },
-    'cobaltIINitrateLowercase': string,
-    'cobaltChlorideLowercase': string,
-    'copperSulfateLowercase': string,
-    'drinkMixLowercase': string,
-    'goldIIIChlorideLowercase': string,
-    'nickelIIChlorideLowercase': string,
-    'potassiumChromateLowercase': string,
-    'potassiumDichromateLowercase': string,
-    'potassiumPermanganateLowercase': string
+    };
+    'cobaltIINitrateLowercase': string;
+    'cobaltChlorideLowercase': string;
+    'copperSulfateLowercase': string;
+    'drinkMixLowercase': string;
+    'goldIIIChlorideLowercase': string;
+    'nickelIIChlorideLowercase': string;
+    'potassiumChromateLowercase': string;
+    'potassiumDichromateLowercase': string;
+    'potassiumPermanganateLowercase': string;
   }
 };
 
