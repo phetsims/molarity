e865597f6bdf72852dfd26236fdce12d06df179e
phet-dev <phetadmin@gmail.com>
2022-07-21 05:13:30 -0600

    update copyright dates from daily grunt work

--- js/molarity/view/describers/PrecipitateAmountDescriber.js
+++ js/molarity/view/describers/PrecipitateAmountDescriber.js
@@ -1,4 +1,4 @@
-// Copyright 2019-2020, University of Colorado Boulder
+// Copyright 2019-2022, University of Colorado Boulder
 
 /**
  * PrecipitateAmountDescriber is responsible for formulating strings about Solution.precipitateAmountProperty. This
--- js/molarity/view/describers/SoluteAmountDescriber.js
+++ js/molarity/view/describers/SoluteAmountDescriber.js
@@ -1,4 +1,4 @@
-// Copyright 2019-2021, University of Colorado Boulder
+// Copyright 2019-2022, University of Colorado Boulder
 
 /**
  * SoluteAmountDescriber is responsible for generating strings about Solution.soluteAmountProperty.
