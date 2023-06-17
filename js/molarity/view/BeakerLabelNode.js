5fbdc9749e5e182afb36942ffd69bbf2785ed762
phet-dev <phetadmin@gmail.com>
2023-06-17 03:33:00 -0600

    update copyright dates from daily grunt work

--- js/molarity/view/BeakerLabelNode.js
+++ js/molarity/view/BeakerLabelNode.js
@@ -1,4 +1,4 @@
-// Copyright 2013-2022, University of Colorado Boulder
+// Copyright 2013-2023, University of Colorado Boulder
 
 /**
  * Label that appears on the beaker in a frosty, translucent frame.
