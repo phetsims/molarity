db07d3a963a8aabb5bd43ea792f6c44fe7a56a04
Michael Kauzmann <michael.kauzmann@colorado.edu>
2024-09-26 14:10:30 -0600

    typo, https://github.com/phetsims/chipper/issues/1474
    Signed-off-by: Michael Kauzmann <michael.kauzmann@colorado.edu>

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,7 +1,7 @@
 // Copyright 2024, University of Colorado Boulder
 
 /**
- * ESlint configuration for molarity.
+ * ESLint configuration for molarity.
  *
  * @author Sam Reid (PhET Interactive Simulations)
  * @author Michael Kauzmann (PhET Interactive Simulations)
