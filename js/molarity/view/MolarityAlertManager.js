8989fa50eb6c525bf00fdc4a9e5d77bde1944878
phet-dev <phetadmin@gmail.com>
2025-03-28 03:26:40 -0600

    update copyright dates from daily grunt work

--- js/molarity/view/MolarityAlertManager.js
+++ js/molarity/view/MolarityAlertManager.js
@@ -1,4 +1,4 @@
-// Copyright 2019-2022, University of Colorado Boulder
+// Copyright 2019-2025, University of Colorado Boulder
 
 /**
  * MolarityAlertManager is responsible for generating and adding alerts to the utteranceQueue. This file is responsible
